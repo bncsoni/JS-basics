08053511f0fa7d3c0df56abd75da170aa38cfb4d
bncsoni <107369560+bncsoni@users.noreply.github.com>
2025-09-22 13:35:22 +0000

    conversion of data type

--- 01_Basics/test.js
+++ 01_Basics/test.js
@@ -1,1 +1,21 @@
-console.log("Banshi")
+// let score = 0
+
+// console.log(typeof score);
+// console.log(typeof(score));
+
+// let valueInNumber = Number(score)
+// console.log(valueInNumber);
+// console.log(typeof(valueInNumber));
+
+let isLoggedIn = NaN
+let boolIsloggedIn = Boolean(typeof(isLoggedIn))
+
+console.log(typeof(isLoggedIn));
+console.log(boolIsloggedIn);
+
+let stringexample = 56
+let stringconv = String(stringexample)
+
+console.log(typeof stringconv);
+
+
